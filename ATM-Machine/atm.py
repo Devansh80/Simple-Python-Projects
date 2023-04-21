707951c141989177db2aa091beff5275b4c737a5
Devansh Rathour <46435891+Devansh80@users.noreply.github.com>
2023-04-21 23:55:32 +0530

    Update atm.py

--- ATM-Machine/atm.py
+++ ATM-Machine/atm.py
@@ -1,41 +1,52 @@
+# Define a class named Account
 class Account:
+    # Define a constructor that initializes name, pin, balance and transaction history attributes
     def __init__(self, name, pin, balance):
         self.name = name
         self.pin = pin
         self.balance = balance
         self.transaction_history = []
-        
+    
+    # Define a method named deposit that allows a user to deposit money    
     def deposit(self, amount):
         self.balance += amount
         self.transaction_history.append(f"Deposit: ${amount}")
         print(f"${amount} deposited. New balance: ${self.balance}")
-        
+    
+    # Define a method named withdraw that allows a user to withdraw money    
     def withdraw(self, amount):
+        # If the withdrawal amount is greater than the current balance, display an error message
         if amount > self.balance:
             print("Insufficient funds.")
+        # If the withdrawal amount is greater than the maximum allowed limit, display an error message
         elif amount > 500:
             print("Withdrawal limit exceeded.")
+        # Otherwise, deduct the withdrawal amount from the balance, add the transaction to the history, and display the updated balance
         else:
             self.balance -= amount
             self.transaction_history.append(f"Withdrawal: ${amount}")
             print(f"${amount} withdrawn. New balance: ${self.balance}")
     
+    # Define a method named check_balance that displays the current balance
     def check_balance(self):
         print(f"Account balance: ${self.balance}")
-        
+    
+    # Define a method named check_history that displays the transaction history    
     def check_history(self):
         print("Transaction history:")
         for transaction in self.transaction_history:
             print(transaction)
 
-
+# Define a main function
 def main():
+    # Prompt the user for name, pin and balance to create an account object
     print("Welcome to the ATM.")
     name = input("Enter your name: ")
     pin = input("Enter your PIN: ")
     balance = int(input("Enter your starting balance: "))
     account = Account(name, pin, balance)
     
+    # Display the menu of options until the user chooses to exit 
     while True:
         print("\n1. Deposit")
         print("2. Withdraw")
@@ -44,6 +55,7 @@ def main():
         print("5. Exit")
         choice = int(input("Enter your choice: "))
         
+        # Call the appropriate method based on the user's choice
         if choice == 1:
             amount = int(input("Enter the amount to deposit: "))
             account.deposit(amount)
@@ -60,11 +72,12 @@ def main():
         else:
             print("Invalid choice. Please try again.")
         
+        # If the transaction history exceeds 5 transactions, end the loop and display a message
         if len(account.transaction_history) > 5:
             print("Withdrawal limit reached.")
             break
 
-
+# Call the main function
 if __name__ == "__main__":
     main()
 
